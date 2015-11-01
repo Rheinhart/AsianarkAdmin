2fa0ec9d69e4ca25db958c1a863f1f561a61714e
Thomas <swiftiii@gmail.com>
2015-11-01 15:23:26 +0800

    refacor livecontroll-Baccarat_Live

--- AsianarkAdmin/livecontroll/tests.py
+++
@@ -1,4 +0,0 @@
-from django.test import TestCase
-import datetime
-# Create your tests here.
-
--- AsianarkAdmin/livecontroll/views.py
+++
@@ -1,7 +0,0 @@
-#coding:utf8
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
-
-def index(request):
-    response = HttpResponseRedirect("/admin")
-    return response
