930bcf70543cc195631cfc23dcdbcc061c60b8ed
Thomas <swiftiii@gmail.com>
2015-11-01 15:25:34 +0800

    refacor livecontroll-Baccarat_Live

--- AsianarkAdmin/settings.py
+++ AsianarkAdmin/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'AsianarkAdmin.adminuser',
     'AsianarkAdmin.userinfo',
-    'AsianarkAdmin.baccarat_Controll',
+    'AsianarkAdmin.Baccarat_Controll',
     'AsianarkAdmin.serverinfo',
 )
 
